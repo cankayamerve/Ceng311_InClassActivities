a09bb04dd34f06e573ca2d6a1518a868835ad760
cankayamerve <153803478+cankayamerve@users.noreply.github.com>
2025-05-08 15:38:05 +0300

    Add files via upload

---
+++ Ustun-Cankaya-Activity11/Ustun-Cankaya-Activity11.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("#nav_list a").click(function (e) {
+        e.preventDefault(); 
+
+        let title = $(this).attr("title"); 
+        let filename = `json_files/${title}.json`; 
+
+        $.getJSON(filename, function (data) {
+            let speaker = data.speakers[0];
+
+            let html = `
+                <h1>${speaker.title}</h1>
+                <img src="${speaker.image}" alt="${speaker.speaker}">
+                <h2>${speaker.month}<br>${speaker.speaker}</h2>
+                <p>${speaker.text}</p>
+            `;
+
+            $("main").html(html);
+        }).fail(function () {
+            $("main").html("<p>An error occurred while retrieving data.</p>");
+        });
+    });
+});
